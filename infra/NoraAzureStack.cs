5d8b3b96ad7c19003af39bcba76b5e2bf9d5e5e6
Andrew Reid <andrew@thinkingbig.net>
2020-12-27 17:00:31 -0400

    Mapping custom domain and cert into web app
    - Adding a custom domain for the web app
    - Adding a cert for the web app
    - Adding the cloudflare pulumi package in order to set custom domain and verification records.

--- infra/NoraAzureStack.cs
+++ infra/NoraAzureStack.cs
@@ -2,6 +2,7 @@ using Pulumi;
 using Pulumi.Azure.AppService;
 using Pulumi.Azure.Core;
 using Pulumi.Azure.CosmosDB;
+using Pulumi.Cloudflare;
 
 class NoraAzureStack : Stack
 {
@@ -17,6 +18,9 @@ class NoraAzureStack : Stack
     var cosmosDbAccount = Account.Get("home-projects-cosmosdb",
       "/subscriptions/72fc2865-939d-4afc-80d4-ab60f14c099e/resourceGroups/Shared/providers/Microsoft.DocumentDB/databaseAccounts/home-projects-cosmosdb");
 
+    // Reference dns zone from cloudflare
+    var dnsZone = Zone.Get("momo-adew.com", "b9eb57d1d01a353e72276d2fa0565d81");
+
     var webApp = new AppService(AddEnvironmentPrefix("nora-web"), new AppServiceArgs
     {
       ResourceGroupName = resourceGroup.Name,
@@ -24,17 +28,45 @@ class NoraAzureStack : Stack
       AppServicePlanId = appServicePlan.Id
     });
 
-    // var hostNameBinding = new CustomHostnameBinding(AddEnvironmentPrefix("nora-web-hostname"), new CustomHostnameBindingArgs
-    // {
-    //   Hostname = AddEnvironmentPrefix("nora.momo-adew.com"),
-    //   AppServiceName = webApp.Name,
-    //   ResourceGroupName = resourceGroup.Name
-    // });
+    var customDomainName = AddEnvironmentPrefix("nora.momo-adew.com");
+    var verificationDomainName = $"asuid.{customDomainName}";
 
-    // var sslCert = new ManagedCertificate("nora-web-cert", new ManagedCertificateArgs
-    // {
-    //   CustomHostnameBindingId = hostNameBinding.Id
-    // });
+    var verificationRecord = new Record(AddEnvironmentPrefix("nora-web-txt-record"), new RecordArgs
+    {
+      Name = verificationDomainName,
+      ZoneId = dnsZone.Id,
+      Type = "TXT",
+      Value = webApp.CustomDomainVerificationId,
+      Ttl = 300
+    });
+
+    var cnameRecord = new Record(AddEnvironmentPrefix("nora-web-cname-record"), new RecordArgs
+    {
+      Name = customDomainName,
+      ZoneId = dnsZone.Id,
+      Type = "CNAME",
+      Value = webApp.DefaultSiteHostname,
+      Ttl = 300
+    });
+
+    var hostNameBinding = new CustomHostnameBinding(AddEnvironmentPrefix("nora-web-hostname"), new CustomHostnameBindingArgs
+    {
+      Hostname = customDomainName,
+      AppServiceName = webApp.Name,
+      ResourceGroupName = resourceGroup.Name
+    });
+
+    var sslCert = new ManagedCertificate("nora-web-cert", new ManagedCertificateArgs
+    {
+      CustomHostnameBindingId = hostNameBinding.Id
+    });
+
+    var sslCertBinding = new CertificateBinding("nora-web-cert-binding", new CertificateBindingArgs
+    {
+      HostnameBindingId = hostNameBinding.Id,
+      CertificateId = sslCert.Id,
+      SslState = "SniEnabled"
+    });
   }
 
   private string AddEnvironmentPrefix(string input)
